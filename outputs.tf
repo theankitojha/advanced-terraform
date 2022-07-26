a739b0a669bd2d68837fd450a8d459d8dc4b8d33
Ankit Ojha <theankitojha@gmail.com>
2022-07-26 14:18:34 +0545

    updated

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # Output variables are like return values, a configuration can have several or none of it
 
 output "instance-dns" {
-  value = aws_instance.ndoejs1.public_dns
+  value = aws_instance.nodejs1.public_dns
 }
